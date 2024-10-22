cfc976387f3ff91bdb0cfce7ce20fb75ca126c0d
Denis Ivanov <dnb8866@gmail.com>
2024-10-22 17:10:53 +0700

    add tests for api

--- src/engine.py
+++ src/engine.py
@@ -3,6 +3,9 @@ from src.utils.db import SqlAlchemyDb
 from src.utils.models_orm import Base
 from src.utils.repositories import ProductRepository, CategoryRepository
 
-sql_db = SqlAlchemyDb(config.SQLALCHEMY_DATABASE_URL_TEST, Base, test=True if config.MODE == 'TEST' else False)
+sql_db = SqlAlchemyDb(
+    config.SQLALCHEMY_DATABASE_URL_TEST if config.MODE == 'TEST' else config.SQLALCHEMY_DATABASE_URL,
+    Base,
+    test=True if config.MODE == 'TEST' else False)
 product_repo = ProductRepository(sql_db)
 category_repo = CategoryRepository(sql_db)
